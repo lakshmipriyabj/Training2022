e198238f5966223889b0c41a4980b5f7cab23513
LakshmiPriya.J <LakshmiPriya.J@EXIND3010-42.allfon.in>
2022-05-20 17:12:10 +0530

    actuator

---
+++ spring-boot-actuator/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/v1")
+@Slf4j
+public class BookController {
+	
+	@GetMapping(path="/books")
+	public List<String> getBooks(){
+		log.info("get books called");
+		
+		return Arrays.asList("Head First java","spring in action","thinking in java");
+	}
+	
+	@GetMapping(path="books/{id}")
+	
+		public String getBookById(@PathVariable int id)
+		{
+			if(id==1)
+			{
+				return "head first java";
+			}else {
+				return "spring in action";
+			}
+		}
+	}
+
+
+
