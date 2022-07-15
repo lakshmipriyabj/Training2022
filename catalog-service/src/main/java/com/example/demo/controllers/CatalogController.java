7eb69335b845abc4e05db652413caeefa2822e26
LakshmiPriya.J <LakshmiPriya.J@EXIND3010-42.allfon.in>
2022-07-15 17:34:02 +0530

    catalog service

---
+++ catalog-service/src/main/java/com/example/demo/controllers/CatalogController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/catalogs")
+public class CatalogController {
+	
+	@GetMapping
+	public HashMap<String, String> getDetails(){
+		
+		HashMap<String,String> details = new HashMap<>();
+		
+		details.put("sedan","Rs.18 per km");
+		details.put("Hatchback", "Rs.14 Per Km");
+		details.put("cycle","Rs.20 Per Km");
+		
+		return details;
+	}
+
+}
