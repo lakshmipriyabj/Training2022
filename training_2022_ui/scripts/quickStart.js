79a3e7393afbd259a071ab615de5e0206f101bee
lakshmipriyabj <95868186+lakshmipriyabj@users.noreply.github.com>
2022-07-21 18:03:08 +0530

    Add files via upload

---
+++ training_2022_ui/scripts/quickStart.js
@@ -0,0 +1,9 @@
+console.log("Hello From java Script")
+
+// var element = document.getElementById("heading"); method invoke by here so in that head tag they wont 
+
+// console.log(element.innerHTML);
+
+function show(){
+    console.log("From Show Function")
+}
